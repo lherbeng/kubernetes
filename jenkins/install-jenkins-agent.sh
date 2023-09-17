61ce81aaa6916117bbb4093e30a4cbeea03ed324
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-09-18 00:11:10 +0800

    Create install-jenkins-agent.sh

---
+++ jenkins/install-jenkins-agent.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Install Java (required for Jenkins agent)
+sudo apt update
+sudo apt install default-jdk -y
+
+# Create a directory for Jenkins agent
+sudo mkdir -p /var/lib/jenkins/agent
+
+# Download Jenkins agent JAR file
+wget http://your-jenkins-server:port/jnlpJars/agent.jar -O /var/lib/jenkins/agent/agent.jar
+# Replace "your-jenkins-server," "port," and "agent-name" with your Jenkins server's information.
+
+# Create a Jenkins agent service file
+sudo tee /etc/systemd/system/jenkins-agent.service <<EOF
+[Unit]
+Description=Jenkins Agent
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/java -jar /var/lib/jenkins/agent/agent.jar -jnlpUrl http://your-jenkins-server:port/computer/agent-name/slave-agent.jnlp
+# Replace "your-jenkins-server," "port," and "agent-name" with your Jenkins server's information.
+
+User=jenkins-agent
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd to pick up the new service
+sudo systemctl daemon-reload
+
+# Start the Jenkins agent service
+sudo systemctl start jenkins-agent
+
+# Enable the Jenkins agent service to start on boot
+sudo systemctl enable jenkins-agent
+
+# Check the status of the Jenkins agent service
+sudo systemctl status jenkins-agent
