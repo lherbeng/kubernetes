ff7075c19b2dd54b90a1fd50b540d46f1b336274
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-10-05 04:48:54 +0800

    Update print_value.py

--- print_value.py
+++ print_value.py
@@ -1,5 +1,9 @@
-# Debugging statement to check the value of $value
-print("Debug: $value =", $value);
+# Replace $value with the actual numerical value
+value = 0.75  # Example numerical value
 
-# Then format and display $value
-formatted_value = printf "%.2f" $value;
+# Debugging statement to check the value of value
+print("Debug: value =", value)
+
+# Then format and display value
+formatted_value = "%.2f" % value
+print("Formatted value:", formatted_value)
