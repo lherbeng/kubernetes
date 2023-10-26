00b65f63143bf21e40b28b8b75ac5b317e8a8144
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-10-27 01:51:56 +0800

    Update script.sh

--- cis-hardening/script.sh
+++ cis-hardening/script.sh
@@ -5,3 +5,16 @@ if [ "$permission" != "644" ]; then
 elif [ "$permission" = "644" ]; then
     echo "Pass"
 fi
+
+---
+
+
+echo "# 1.1 Master Node Configuration Files"
+
+# 1.1.1
+permission=$(stat -c %a /var/lib/rancher/rke2/agent/pod-manifests/kube-apiserver.yaml)
+if [ "$permission" != "644" ]; then
+    echo "Current permission: $permission. Result: Fail"
+elif [ "$permission" = "644" ]; then
+    echo "Current permission: $permission. Result: Pass"
+fi
