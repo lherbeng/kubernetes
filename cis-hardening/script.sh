83894d8fb04d5c9b427a5d9bd87b0a2269cf6928
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-10-27 02:20:26 +0800

    Update script.sh

--- cis-hardening/script.sh
+++ cis-hardening/script.sh
@@ -2,7 +2,7 @@
 
 # 1.1 Master Node Configuration Files
 
-echo "# 1.1.1 Ensure that the API server pod specification file permissions are set to 644 or more restrictive (Automated)"
+echo "1.1.1 Ensure that the API server pod specification file permissions are set to 644 or more restrictive (Automated)"
 
 permission=$(stat -c %a /var/lib/rancher/rke2/agent/pod-manifests/kube-apiserver.yaml)
 if [ "$permission" != "644" ]; then
@@ -11,7 +11,9 @@ elif [ "$permission" = "644" ]; then
     echo "Current permission: $permission. Result: Pass"
 fi
 
-echo "# 1.1.2 Ensure that the API server pod specification file ownership is set to root:root (Automated)"
+echo
+
+echo "1.1.2 Ensure that the API server pod specification file ownership is set to root:root (Automated)"
 ownership=$(stat -c %U:%G /var/lib/rancher/rke2/agent/pod-manifests/kube-apiserver.yaml)
 if [ "$ownership" != "root:root" ]; then
     echo "Current ownership: $ownership. Result: Fail"
@@ -19,13 +21,26 @@ elif [ "$ownership" = "root:root" ]; then
     echo "Current ownership: $ownership. Result: Pass"
 fi
 
-echo "# 1.1.3 Ensure that the controller manager pod specification file permissions are set to 644 or more restrictive (Automated)"
+echo
+
+echo "1.1.3 Ensure that the controller manager pod specification file permissions are set to 644 or more restrictive (Automated)"
 
 permission=$(stat -c %a /var/lib/rancher/rke2/agent/pod-manifests/kube-controller-manager.yaml)
 if [ "$permission" != "644" ]; then
     echo "Current permission: $permission. Result: Fail"
 elif [ "$permission" = "644" ]; then
     echo "Current permission: $permission. Result: Pass"
+fi
+
+echo
+
+echo "1.1.4 Ensure that the controller manager pod specification file ownership is set to root:root (Automated)"
+
+ownership=$(stat -c %U:%G /var/lib/rancher/rke2/agent/pod-manifests/kube-controller-manager.yaml)
+if [ "$ownership" != "root:root" ]; then
+    echo "Current ownership: $ownership. Result: Fail"
+elif [ "$ownership" = "root:root" ]; then
+    echo "Current ownership: $ownership. Result: Pass"
 fi
 
 
