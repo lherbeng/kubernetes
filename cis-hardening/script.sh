3aa28a6efe851774a65686c66012bcae9319c19a
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-10-27 02:14:11 +0800

    Update script.sh

--- cis-hardening/script.sh
+++ cis-hardening/script.sh
@@ -6,25 +6,26 @@ echo "# 1.1.1 Ensure that the API server pod specification file permissions are 
 
 permission=$(stat -c %a /var/lib/rancher/rke2/agent/pod-manifests/kube-apiserver.yaml)
 if [ "$permission" != "644" ]; then
-    echo "Fail"
+    echo "Current permission: $permission. Result: Fail"
 elif [ "$permission" = "644" ]; then
-    echo "Pass"
+    echo "Current permission: $permission. Result: Pass"
 fi
 
 echo "# 1.1.2 Ensure that the API server pod specification file ownership is set to root:root (Automated)"
 ownership=$(stat -c %U:%G /var/lib/rancher/rke2/agent/pod-manifests/kube-apiserver.yaml)
-
-if [ "$ownership" = "root:root" ]; then
-    echo "Pass"
-else
-    echo "Fail"
+if [ "$ownership" != "root:root" ]; then
+    echo "Current ownership: $ownership. Result: Fail"
+elif [ "$ownership" = "root:root" ]; then
+    echo "Current ownership: $ownership. Result: Pass"
 fi
 
 echo "# 1.1.3 Ensure that the controller manager pod specification file permissions are set to 644 or more restrictive (Automated)"
 
 permission=$(stat -c %a /var/lib/rancher/rke2/agent/pod-manifests/kube-controller-manager.yaml)
 if [ "$permission" != "644" ]; then
-    echo "Fail"
+    echo "Current permission: $permission. Result: Fail"
 elif [ "$permission" = "644" ]; then
-    echo "Pass"
+    echo "Current permission: $permission. Result: Pass"
 fi
+
+
