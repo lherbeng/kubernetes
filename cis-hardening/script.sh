cd5014171cad47734386684674409eb86acd6330
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-10-27 04:44:49 +0800

    Update script.sh

--- cis-hardening/script.sh
+++ cis-hardening/script.sh
@@ -233,125 +233,187 @@ echo "1.2.2 Ensure that the --token-auth-file parameter is not set (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep | grep -o -- --token-auth-file
 
+echo
+
 echo "1.2.3 Ensure that the --DenyServiceExternalIPs is not set (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep | grep -o -- --DenyServiceExternalIPs
+
+echo
 
 echo "1.2.4 Ensure that the --kubelet-https argument is set to true (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep | grep -o -- --kubelet-https=true
 
+echo
+
 echo "1.2.5 Ensure that the --kubelet-client-certificate and --kubelet-client-key arguments are set as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.6 Ensure that the --kubelet-certificate-authority argument is set as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.7 Ensure that the --authorization-mode argument is not set to AlwaysAllow (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.8 Ensure that the --authorization-mode argument includes Node (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.9 Ensure that the --authorization-mode argument includes RBAC (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.10 Ensure that the admission control plugin EventRateLimit is set (Manual)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.11 Ensure that the admission control plugin AlwaysAdmit is not set (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.12 Ensure that the admission control plugin AlwaysPullImages is set (Manual)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.13 Ensure that the admission control plugin SecurityContextDeny is set if PodSecurityPolicy is not used (Manual)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.14 Ensure that the admission control plugin ServiceAccount is set (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.15 Ensure that the admission control plugin NamespaceLifecycle is set (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.16 Ensure that the admission control plugin NodeRestriction is set (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.17 Ensure that the --secure-port argument is not set to 0 (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.18 Ensure that the --profiling argument is set to false (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.19 Ensure that the --audit-log-path argument is set (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.20 Ensure that the --audit-log-maxage argument is set to 30 or as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.21 Ensure that the --audit-log-maxbackup argument is set to 10 or as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.22 Ensure that the --audit-log-maxsize argument is set to 100 or as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.23 Ensure that the --request-timeout argument is set as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.24 Ensure that the --service-account-lookup argument is set to true (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.25 Ensure that the --service-account-key-file argument is set as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.26 Ensure that the --etcd-certfile and --etcd-keyfile arguments are set as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.27 Ensure that the --tls-cert-file and --tls-private-key-file arguments are set as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.28 Ensure that the --client-ca-file argument is set as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.29 Ensure that the --etcd-cafile argument is set as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 echo "1.2.30 Ensure that the --encryption-provider-config argument is set as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
 
+echo
+
 echo "1.2.31 Ensure that encryption providers are appropriately configured (Automated)"
 
 grep aescbc /var/lib/rancher/rke2/server/cred/encryption-config.json
 
+echo
+
 echo "1.2.32 Ensure that the API Server only makes use of Strong Cryptographic Ciphers (Manual)"
 
 /bin/ps -ef | grep kube-apiserver | grep -v grep
+
+echo
 
 # 1.3 Controller Manager
 
@@ -359,29 +421,43 @@ echo "1.3.1 Ensure that the --terminated-pod-gc-threshold argument is set as app
 
 /bin/ps -ef | grep kube-controller-manager | grep -v grep
 
+echo
+
 echo "1.3.2 Ensure that the --profiling argument is set to false (Automated)"
 
 /bin/ps -ef | grep kube-controller-manager | grep -v grep
+
+echo
 
 echo "1.3.3 Ensure that the --use-service-account-credentials argument is set to true (Automated)"
 
 /bin/ps -ef | grep kube-controller-manager | grep -v grep
 
+echo
+
 echo "1.3.4 Ensure that the --service-account-private-key-file argument is set as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-controller-manager | grep -v grep
+
+echo
 
 echo "1.3.5 Ensure that the --root-ca-file argument is set as appropriate (Automated)"
 
 /bin/ps -ef | grep kube-controller-manager | grep -v grep
 
+echo
+
 echo "1.3.6 Ensure that the RotateKubeletServerCertificate argument is set to true (Automated)"
 
 /bin/ps -ef | grep kube-controller-manager | grep -v grep
 
+echo
+
 echo "1.3.7 Ensure that the --bind-address argument is set to 127.0.0.1 (Automated)"
 
 /bin/ps -ef | grep kube-controller-manager | grep -v grep
+
+echo
 
 # 1.4 Scheduler
 
@@ -394,6 +470,8 @@ echo
 echo "1.4.2 Ensure that the --bind-address argument is set to 127.0.0.1 (Automated)"
 
 /bin/ps -ef | grep kube-scheduler | grep -v grep
+
+echo
 
 # 2 Etcd Node Configuration
 
@@ -463,6 +541,8 @@ echo "3.2.2 Ensure that the audit policy covers key security concerns (Manual)"
 
 echo "Rationale: Security audit logs should cover access and modification of key resources in the cluster, to enable them to form an effective part of a security environment."
 
+echo
+
 # 4 Worker Node Security Configuration
 
 echo "4.1.1 Ensure that the kubelet service file permissions are set to 644 or more restrictive (Automated)"
@@ -509,7 +589,6 @@ elif [ "$permission" = "644" ]; then
 fi
 
 echo 
-
 
 echo "4.1.6 Ensure that the kubelet.conf file ownership is set to root:root (Manual)"
 
@@ -695,6 +774,198 @@ echo "5.1.7 Limit use of the Bind, Impersonate and Escalate permissions in the K
 echo
 
 # 5.2 Pod Security Standards
+
+echo "5.2.1 Ensure that the cluster has at least one active policy control mechanism in place (Manual)"
+
+echo "Remediation: PSA is enabled since v1.23 by default in RKE2, no remediation necessary."
+
+echo
+
+echo "5.2.2 Minimize the admission of privileged containers (Manual)"
+
+config_file=$(ps aux | grep kube-apiserver |  grep -- --admission-control-config-file | sed 's%.*admission-control-config-file[= ]\([^ ]*\).*%\1%')
+
+grep "enforce:" ${config_file}
+
+echo "Verify that the returned value is enforce: restricted"
+
+echo
+
+echo "5.2.3 Minimize the admission of containers wishing to share the host process ID namespace (Automated)"
+
+config_file=$(ps aux | grep kube-apiserver |  grep -- --admission-control-config-file | sed 's%.*admission-control-config-file[= ]\([^ ]*\).*%\1%')
+
+grep "enforce:" ${config_file}
+
+echo "Verify that the returned value is enforce: restricted"
+
+echo
+
+echo "5.2.4 Minimize the admission of containers wishing to share the host IPC namespace (Automated)"
+
+config_file=$(ps aux | grep kube-apiserver |  grep -- --admission-control-config-file | sed 's%.*admission-control-config-file[= ]\([^ ]*\).*%\1%')
+
+grep "enforce:" ${config_file}
+
+echo "Verify that the returned value is enforce: restricted"
+
+echo
+
+echo "5.2.5 Minimize the admission of containers wishing to share the host network namespace (Automated)"
+
+config_file=$(ps aux | grep kube-apiserver |  grep -- --admission-control-config-file | sed 's%.*admission-control-config-file[= ]\([^ ]*\).*%\1%')
+
+grep "enforce:" ${config_file}
+
+echo "Verify that the returned value is enforce: restricted"
+
+echo
+
+echo "5.2.6 Minimize the admission of containers with allowPrivilegeEscalation (Automated)"
+
+config_file=$(ps aux | grep kube-apiserver |  grep -- --admission-control-config-file | sed 's%.*admission-control-config-file[= ]\([^ ]*\).*%\1%')
+
+grep "enforce:" ${config_file}
+
+echo "Verify that the returned value is enforce: restricted"
+
+echo
+
+echo "5.2.7 Minimize the admission of root containers (Automated)"
+
+config_file=$(ps aux | grep kube-apiserver |  grep -- --admission-control-config-file | sed 's%.*admission-control-config-file[= ]\([^ ]*\).*%\1%')
+
+grep "enforce:" ${config_file}
+
+echo "Verify that the returned value is enforce: restricted"
+
+echo
+
+echo "5.2.8 Minimize the admission of containers with the NET_RAW capability (Automated)"
+
+config_file=$(ps aux | grep kube-apiserver |  grep -- --admission-control-config-file | sed 's%.*admission-control-config-file[= ]\([^ ]*\).*%\1%')
+
+grep "enforce:" ${config_file}
+
+echo "Verify that the returned value is enforce: restricted"
+
+echo
+
+echo "5.2.9 Minimize the admission of containers with added capabilities (Automated)"
+
+echo "Remediation: Ensure that allowedCapabilities is not present in policies for the cluster unless it is set to an empty array."
+
+echo
+
+echo "5.2.10 Minimize the admission of containers with capabilities assigned (Manual)"
+
+echo "Remediation: Review the use of capabilities in applications running on your cluster. Where a namespace contains applications which do not require any Linux capabilities to operate consider adding a PSP which forbids the admission of containers which do not drop all capabilities."
+
+echo
+
+echo "5.2.11 Minimize the admission of Windows HostProcess containers (Manual)"
+
+echo "Remediation: Add policies to each namespace in the cluster which has user workloads to restrict the admission of containers that have .securityContext.windowsOptions.hostProcess set to true."
+
+echo
+
+echo "5.2.12 Minimize the admission of HostPath volumes (Manual)"
+
+echo "Remediation: Add policies to each namespace in the cluster which has user workloads to restrict the admission of containers with hostPath volumes."
+
+echo
+
+echo "5.2.13 Minimize the admission of containers which use HostPorts (Manual)"
+
+echo "Remediation: Add policies to each namespace in the cluster which has user workloads to restrict the admission of containers which use hostPort sections"
+
+echo 
+
+# 5.3 Network Policies and CNI
+
+echo "5.3.1 Ensure that the CNI in use supports Network Policies (Automated)"
+
+echo "Remediation: By default, RKE2 use Canal (Calico and Flannel) and fully supports network policies."
+
+echo
+
+echo "5.3.2 Ensure that all Namespaces have Network Policies defined (Automated)"
+
+for i in kube-system kube-public default; do
+    /var/lib/rancher/rke2/bin/kubectl get networkpolicies -n $i;
+done
+
+echo "Verify that there are network policies applied to each of the namespaces"
+
+echo
+
+# 5.4 Secrets Management 
+
+echo "5.4.1 Prefer using secrets as files over secrets as environment variables (Manual)"
+
+/var/lib/rancher/rke2/bin/kubectl get all -o jsonpath='{range .items[?(@..secretKeyRef)]} {.kind} {.metadata.name} {"\n"}{end}' -A
+
+echo
+
+echo "5.4.2 Consider external secret storage (Manual)"
+
+echo "Audit: Review your secrets management implementation."
+
+echo "Remediation: Refer to the secrets management options offered by your cloud provider or a third-party secrets management solution."
+
+echo
+
+# 5.5 Extensible Admission Control
+
+echo "5.5.1 Configure Image Provenance using ImagePolicyWebhook admission controller (Manual)"
+
+echo "Audit: Review the pod definitions in your cluster and verify that image provenance is configured as appropriate."
+
+echo "Remediation: Follow the Kubernetes documentation and setup image provenance."
+
+echo
+
+# 5.6 Omitted
+
+echo "The v1.23 guide skips 5.6 and goes from 5.5 to 5.7. We are including it here merely for explanation."
+
+echo
+
+# 5.7 General Policies
+
+echo "5.7.1 Create administrative boundaries between resources using namespaces (Manual)"
+
+/var/lib/rancher/rke2/bin/kubectl get namespaces
+
+echo
+
+echo "5.7.2 Ensure that the seccomp profile is set to docker/default in your pod definitions (Manual)"
+
+echo "Audit: Review the pod definitions in your cluster. It should create a line as below:"
+
+echo "annotations:
+  seccomp.security.alpha.kubernetes.io/pod: docker/default"
+
+echo "Remediation: Review the Kubernetes documentation and if needed, apply a relevant PodSecurityPolicy."
+
+echo
+
+echo "5.7.3 Apply Security Context to Your Pods and Containers (Manual)"
+
+echo "Audit: Review the pod definitions in your cluster and verify that you have security contexts defined as appropriate."
+
+echo "Remediation: Follow the Kubernetes documentation and apply security contexts to your pods. For a suggested list of security contexts, you may refer to the CIS Security Benchmark."
+
+echo
+
+echo "5.7.4 The default namespace should not be used (Manual)"
+
+/var/lib/rancher/rke2/bin/kubectl get all -n default
+
+
+
+
+
 
 
 
