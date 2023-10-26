0fd09267d838ad339a1a4d6d93fcc4e15449d8a5
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-10-27 02:33:28 +0800

    Update script.sh

--- cis-hardening/script.sh
+++ cis-hardening/script.sh
@@ -47,7 +47,7 @@ echo
 
 echo "1.1.5 Ensure that the scheduler pod specification file permissions are set to 644 or more restrictive (Automated)"
 
-ownership=$(stat -c %a /var/lib/rancher/rke2/agent/pod-manifests/kube-scheduler.yaml)
+permission=$(stat -c %a /var/lib/rancher/rke2/agent/pod-manifests/kube-scheduler.yaml)
 if [ "$permission" != "644" ]; then
     echo "Current permission: $permission. Result: Fail"
 elif [ "$permission" = "644" ]; then
@@ -67,9 +67,49 @@ fi
 
 echo
 
+echo "1.1.7 Ensure that the etcd pod specification file permissions are set to 644 or more restrictive (Automated)"
 
+permission=$(stat -c %a /var/lib/rancher/rke2/agent/pod-manifests/etcd.yaml)
+if [ "$permission" != "644" ]; then
+    echo "Current permission: $permission. Result: Fail"
+elif [ "$permission" = "644" ]; then
+    echo "Current permission: $permission. Result: Pass"
+fi
 
+echo 
 
+echo "1.1.8 Ensure that the etcd pod specification file ownership is set to root:root (Automated)"
+
+ownership=$(stat -c %U:%G /var/lib/rancher/rke2/agent/pod-manifests/etcd.yaml)
+if [ "$ownership" != "root:root" ]; then
+    echo "Current ownership: $ownership. Result: Fail"
+elif [ "$ownership" = "root:root" ]; then
+    echo "Current ownership: $ownership. Result: Pass"
+fi
+
+echo
+
+echo "1.1.9 Ensure that the Container Network Interface file permissions are set to 644 or more restrictive (Manual)"
+
+permission=$(stat -c %a /var/lib/rancher/rke2/server/manifests/rke2-canal.yaml)
+if [ "$permission" != "644" ]; then
+    echo "Current permission: $permission. Result: Fail"
+elif [ "$permission" = "644" ]; then
+    echo "Current permission: $permission. Result: Pass"
+fi
+
+echo
+
+echo "1.1.10 Ensure that the Container Network Interface file ownership is set to root:root (Manual)"
+
+ownership=$(stat -c %U:%G /var/lib/rancher/rke2/server/manifests/rke2-canal.yaml)
+if [ "$ownership" != "root:root" ]; then
+    echo "Current ownership: $ownership. Result: Fail"
+elif [ "$ownership" = "root:root" ]; then
+    echo "Current ownership: $ownership. Result: Pass"
+fi
+
+echo
 
 
 
