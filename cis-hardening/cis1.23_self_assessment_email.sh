e0eac30bedd3fe15c61bd024a38c6690873442b4
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-11-08 14:51:54 +0800

    Update cis1.23_self_assessment_email.sh

--- cis-hardening/cis1.23_self_assessment_email.sh
+++ cis-hardening/cis1.23_self_assessment_email.sh
@@ -1,59 +1,33 @@
 #!/bin/bash
 
-{
+# Get the current hostname
+current_hostname=$(hostname)
 
-# 1.1 Master Node Configuration Files
+# Function to send email for a specific hostname
+send_email() {
+    hostname=$1
+    recipient="recipient@example.com"  # Replace with the actual recipient email address
+    subject="Self-Assessment Summary for $hostname"
+    body="See attached files for the self-assessment summary of $hostname."
 
-echo "1.1.1 Ensure that the API server pod specification file permissions are set to 644 or more restrictive (Automated)" >> "self-assessment_failed_$(hostname).txt"
+    # Create a list of all .txt files
+    cd /d3/data01/cishardening/ || exit
+    attachments=""
 
-permission=$(stat -c %a /var/lib/rancher/rke2/agent/pod-manifests/kube-apiserver.yaml)
-if [ "$permission" != "644" ]; then
-    echo "Current permission: $permission. Result: Fail" >> "self-assessment_failed_$(hostname).txt"
-elif [ "$permission" = "644" ]; then
-    echo "Current permission: $permission. Result: Pass"
+    for file in *.txt; do
+        attachments+=" -a $file"
+    done
+
+    # Send email with all attachments
+    echo -e "$body" | mailx -s "$subject" $attachments $recipient
+
+    # Delete files in the directory
+    rm -f ./*"${hostname}"*.txt
+}
+
+# Check if the current hostname matches the predefined hostname and send the email
+if [ "$current_hostname" == "hostnameA" ] || [ "$current_hostname" == "hostnameB" ] || [ "$current_hostname" == "hostnameC" ] || [ "$current_hostname" == "hostnameD" ]; then
+    send_email "$current_hostname"
+else
+    echo "Current hostname not found in the predefined hostnames."
 fi
-
-echo
-
-echo "1.1.2 Ensure that the API server pod specification file ownership is set to root:root (Automated)" >> "self-assessment_failed_$(hostname).txt"
-ownership=$(stat -c %U:%G /var/lib/rancher/rke2/agent/pod-manifests/kube-apiserver.yaml)
-if [ "$ownership" != "root:root" ]; then
-    echo "Current ownership: $ownership. Result: Fail" >> "self-assessment_failed_$(hostname).txt"
-elif [ "$ownership" = "root:root" ]; then
-    echo "Current ownership: $ownership. Result: Pass"
-fi
-
-} >> "self-assessment_summary_$(hostname).txt" 2>&1
-
-grep "Fail" "self-assessment_summary_$(hostname).txt" >> "self-assessment_failed_$(hostname).txt"
-
-if [ ! -d "/d3/data01/cishardening" ]; then
-    mkdir -p "/d3/data01/cishardening"
-fi
-
-mv "self-assessment_summary_$(hostname).txt" "/d3/data01/cishardening/"
-mv "self-assessment_failed_$(hostname).txt" "/d3/data01/cishardening/"
-
-
-
----
-
-#!/bin/bash
-
-# Create a list of all .txt files
-cd /d3/data01/cishardening/
-attachments=""
-
-for file in *txt; do
-    attachments+=" -a $file"
-done
-
-# Send email with all attachments
-recipient="recipient@example.com"
-subject="Self-Assessment Summary"
-body="See attached files."
-
-echo -e "$body" | mailx -s "$subject" $attachments $recipient
-
-# Delete files in the directory
-rm -f /d3/data01/cishardening/*.txt
