cbbc48ebe46aa0de81e29387dbf6ff3e85fb1499
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-11-10 10:06:21 +0800

    Update new_ideas.sh

--- cis-hardening/new_ideas.sh
+++ cis-hardening/new_ideas.sh
@@ -1,44 +1,39 @@
-stage('SANDBOX-DEV Install') {
-    options { skipDefaultCheckout(false) }
-    when {
-        expression {
-            params.Action == "Install" && params.Hostname != "" && params.Environment == "sandbox"
-        }
-    }
-    agent { label "on-prem-dev-static-worker-jenkins" }
+#!/bin/bash
 
-    steps {
-        Script {
-            if (params.Hostname != "") {
-                sh """
-                    sed -i 's/HostameParam/${params.Hostname}/' `pwd`/infra/scripts/cis1.23_self_assessment.sh
-                """
-                // Add a delay to ensure the self-assessment script has run on all nodes
-                sleep 300 // Adjust the time interval as needed
+# Define the current hostname and subject variables
+current_hostname=$(hostname)
+subject_variable="Sandbox"
 
-                // Place the self-assessment function here
-                selfAssessment("${params.Hostname}","$params.ADUsername}")
-            }
-        }
-    }
+# Function to send email for a specific hostname
+send_email() {
+    hostname=$1
+    recipient="recipient@example.com"  # Replace with the actual recipient email address
+    if [ "$hostname" == "$current_hostname" ]; then
+        subject="Self-Assessment Summary for $subject_variable"
+        body="See attached files for the self-assessment summary of $subject_variable."
+    else
+        subject="Self-Assessment Summary for $hostname"
+        body="See attached files for the self-assessment summary of $hostname."
+    fi
+
+    # Create a list of all .txt files
+    cd /d3/data01/cishardening/ || exit
+    attachments=""
+
+    for file in *.txt; do
+        attachments+=" -a $file"
+    done
+
+    # Send email with all attachments
+    echo -e "$body" | mailx -s "$subject" $attachments $recipient
+
+    # Delete files in the directory
+    rm -f ./*"${hostname}"*.txt
 }
 
-stage('Run email script') {
-    when {
-        expression {
-            currentBuild.rawBuild.getWorkspace().child('lock.file').exists()
-        }
-    }
-    steps {
-        sh """
-            sed -i 's/HostameParam/${params.Hostname}/' `pwd`/infra/scripts/cis1.23_self_assessment_email.sh
-        """
-    }
-    post {
-        always {
-            script {
-                currentBuild.rawBuild.getWorkspace().child('lock.file').delete()
-            }
-        }
-    }
-}
+# Check if the current hostname matches the predefined hostname and send the email
+if [ "$current_hostname" == "hostnameA" ] || [ "$current_hostname" == "hostnameB" ] || [ "$current_hostname" == "hostnameC" ] || [ "$current_hostname" == "hostnameD" ]; then
+    send_email "$current_hostname"
+else
+    echo "Current hostname not found in the predefined hostnames."
+fi
