120f4e3eb2d6941b71606bb9af13be4a7e9f5aa8
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-11-12 17:38:25 +0800

    Update cis1.23_self_assessment_email.sh

--- cis-hardening/cis1.23_self_assessment_email.sh
+++ cis-hardening/cis1.23_self_assessment_email.sh
@@ -5,24 +5,25 @@ current_hostname=$(hostname)
 
 # Function to send email for a specific hostname
 send_email() {
-    hostname=$1
-    recipient="recipient@example.com"  # Replace with the actual recipient email address
-    subject="Self-Assessment Summary for $hostname"
-    body="See attached files for the self-assessment summary of $hostname."
+hostname=$1
+recipient="recipient@example.com"  # Replace with the actual recipient email address
+subject="Self-Assessment Summary for $hostname"
+body="See attached files for the self-assessment summary of $hostname."
 
-    # Create a list of all .txt files
-    cd /d3/data01/cishardening/ || exit
-    attachments=""
+# Create a list of all .txt files
+cd /d3/data01/cishardening/ || exit
+attachments=""
 
-    for file in *.txt; do
-        attachments+=" -a $file"
-    done
+for file in *.txt; do
+    attachments+=" -a $file"
+done
 
-    # Send email with all attachments
-    echo -e "$body" | mailx -s "$subject" $attachments $recipient
+# Send email with all attachments
+echo -e "$body" | mailx -s "$subject" $attachments $recipient
 
-    # Delete files in the directory
-    rm -f /d3/data01/cishardening/*.txt
+# Delete files in the directory
+rm -f /d3/data01/cishardening/*.txt
+
 }
 
 # Check if the current hostname matches the predefined hostname and send the email
