f87e9c0cd4a4686b6f11a03338c738470ffb07d7
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-11-24 10:48:49 +0800

    Update new_ideas.sh

--- cis-hardening/new_ideas.sh
+++ cis-hardening/new_ideas.sh
@@ -78,3 +78,66 @@ else
 fi
 
 # Rest of your script...
+
+---
+
+#!/bin/bash
+
+# Define the current hostname
+current_hostname=$(hostname)
+
+# Define the subject variable based on the current hostname
+if [ "$current_hostname" == "hostnameA" ]; then
+    subject_variable="SANDBOX"
+elif [ "$current_hostname" == "hostnameB" ]; then
+    subject_variable="DEV"
+elif [ "$current_hostname" == "hostnameC" ]; then
+    subject_variable="TST"
+elif [ "$current_hostname" == "hostnameD" ]; then
+    subject_variable="PROD"
+else
+    # Default subject variable if the current hostname doesn't match predefined ones
+    subject_variable="UNKNOWN"
+fi
+
+# Function to send email for a specific hostname
+send_email() {
+    hostname=$1
+    recipient="recipient@example.com"  # Replace with the actual recipient email address
+    if [ "$hostname" == "$current_hostname" ]; then
+        subject="Self-Assessment Summary for $subject_variable"
+        body="See attached files for the self-assessment summary of $subject_variable."
+    else
+        subject="Self-Assessment Summary for $hostname"
+        body="See attached files for the self-assessment summary of $hostname."
+    fi
+
+    # List .txt files in both directories
+    attachments=""
+    
+    # Check /d3/data01/cishardening/
+    if [ -d "/d3/data01/cishardening/" ]; then
+        cd /d3/data01/cishardening/ || exit
+        for file in *.txt; do
+            attachments+=" -a $file"
+        done
+    fi
+
+    # Check /d1/data01/cishardening/
+    if [ -d "/d1/data01/cishardening/" ]; then
+        cd /d1/data01/cishardening/ || exit
+        for file in *.txt; do
+            attachments+=" -a $file"
+        done
+    fi
+
+    # Send email with all attachments
+    echo -e "$body" | mailx -s "$subject" $attachments $recipient
+}
+
+# Check if the current hostname matches the predefined hostname and send the email
+if [ "$current_hostname" == "hostnameA" ] || [ "$current_hostname" == "hostnameB" ] || [ "$current_hostname" == "hostnameC" ] || [ "$current_hostname" == "hostnameD" ]; then
+    send_email "$current_hostname"
+else
+    echo "Current hostname not found in the predefined hostnames."
+fi
