efb30223e4734127b030cfef17406703532def13
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2023-12-01 12:09:58 +0800

    Create alertingsample.tf

---
+++ alerting-rules/alertingsample.tf
@@ -0,0 +1,91 @@
+conditions {
+  - evaulator {
+      params = []
+      type   = "gt"
+    }
+  operator {
+    type = "and"
+  }
+  query {
+    params = [
+      "B"
+    ]
+  }
+  reducer {
+    params = []
+    type   = "last"
+  }
+  type = "query"
+}
+
+apiVersion = 1
+groups = [
+  {
+    origId   = 1
+    name     = "FilesystemUsageTooHigh metrics"
+    folder   = "Nodes Monitoring"
+    interval = "5m"
+    rules    = [
+      {
+        uid       = "c9d8a183-b0b7-4b46-8cbb-9e19b7474995"
+        title     = "Filesystem usage too high metrics"
+        condition = "B"
+        data      = [
+          {
+            refId              = "A"
+            relativeTimeRange = {
+              from = 600
+              to   = 0
+            }
+            datasourceUid = "prometheus"
+            model         = {
+              editorMode   = "code"
+              expr         = '100 - (node_filesystem_free_bytes{device="/dev/mapper/vg_data-rke2"} / node_filesystem_size_bytes{device="/dev/mapper/vg_data-rke2"}) * 100 > 85'
+              instant      = true
+              intervalMs   = 1000
+              legendFormat = "__auto"
+              maxDataPoints = 43200
+              range        = false
+              refId        = "A"
+            }
+          },
+          {
+            refId              = "B"
+            relativeTimeRange = {
+              from = 600
+              to   = 0
+            }
+            datasourceUid = "__expr__"
+          }
+        ]
+      }
+    ]
+  }
+]
+
+datasource {
+  type         = "__expr__"
+  uid          = "__expr__"
+  expression   = "A"
+  intervalMs   = 1000
+  maxDataPoints = 43200
+  reducer      = "last"
+  refId        = "B"
+  type         = "reduce"
+}
+
+noDataState   = "Ok"
+execErrstate  = "Alerting"
+for           = "5m"
+
+annotations = {
+  description = 'Attention: The disk usage for /var/lib/rancher has reached 85% capacity, requiring immediate attention.'
+  runbook_url = "https://confluence.citco.com/display/KUB/Grafana+Alert+-+FileSystemUsageTooHigh"
+  summary     = "Filesystem usage is too high"
+}
+
+labels = {
+  severity = "critical"
+}
+
+isPaused = false
