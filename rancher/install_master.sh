a2c6e9c7e4fd8b8248f5f5f9dd4cc269964b77cd
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2024-02-10 11:14:11 +0800

    Update install_master.sh

--- rancher/install_master.sh
+++ rancher/install_master.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
 
-
+# Generate a random token
+RKE2_TOKEN=$(openssl rand -base64 32)
 
 # Create RKE2 Directory
 mkdir -p /etc/rancher/rke2/
 
-#
+# Create or overwrite the RKE2 configuration file with the generated token
 
-#
-
-cat << EOF >> /etc/rancher/rke2/config.yaml
-token: rke2SecurePassword
+cat << EOF > /etc/rancher/rke2/config.yaml
+token: $RKE2_TOKEN
 EOF
