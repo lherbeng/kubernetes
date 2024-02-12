6f1968c096d0a265c9c10b039ae7e600f9c2f719
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2024-02-12 10:55:40 +0800

    Update install_rke2_agent.sh

--- rancher/install_rke2_agent.sh
+++ rancher/install_rke2_agent.sh
@@ -14,4 +14,10 @@ apt autoremove -y
 # Install RKE2 agent
 curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE=agent sh -
 
-# 
+# Create RKE2 config Directory
+mkdir -p /etc/rancher/rke2/
+
+# Create config.yaml file for RKE2 agent
+vi /etc/rancher/rke2/config.yaml
+server:https//RKE2_SERVER_IP_ADDRESS:9345
+token:TOKEN_FROM_RKE2_SERVER
