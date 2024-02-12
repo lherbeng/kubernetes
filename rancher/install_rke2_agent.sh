b65dd1b95cc7c35ff89df581d29fc86665b419ad
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2024-02-12 10:46:58 +0800

    Update install_rke2_agent.sh

--- rancher/install_rke2_agent.sh
+++ rancher/install_rke2_agent.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+# Stop Firewall
+systemctl disable --now ufw
+
+# Run updates and upgrade and install packages
+apt update
+apt install nfs-common open-iscsi -y
+apt upgrade -y
+
+# Automatic removal of unnecessary packages
+apt autoremove -y
+
+# Install RKE2 agent
+curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE=agent sh -
+
+# 
