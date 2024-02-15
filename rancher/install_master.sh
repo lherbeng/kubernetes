ef05b6002cf98729685827eebb34d17bcfd73048
Lherben Gesite <72662912+lherbeng@users.noreply.github.com>
2024-02-15 17:28:46 +0800

    Update install_master.sh

--- rancher/install_master.sh
+++ rancher/install_master.sh
@@ -11,18 +11,6 @@ apt upgrade -y
 # Automatic removal of unnecessary packages
 apt autoremove -y
 
-# Generate a random token
-RKE2_TOKEN=$(openssl rand -base64 32)
-
-# Create RKE2 Directory
-mkdir -p /etc/rancher/rke2/
-
-# Create or overwrite the RKE2 configuration file with the generated token
-
-cat << EOF > /etc/rancher/rke2/config.yaml
-token: $RKE2_TOKEN
-EOF
-
 # Install RKE2 server
 curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE=server sh -
 
